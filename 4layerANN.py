5b96eaa03786f7ea2e617d154f6d34d3b72ea98e
Neithardt-zn <bf3nbzn21cn@gmail.com>
2017-04-24 10:03:13 +0800

    Create 4layerANN.py

---
+++ 4layerANN.py
@@ -0,0 +1,114 @@
+import numpy as np
+from sklearn import datasets, linear_model
+import matplotlib.pyplot as plt
+
+class Config:
+    nn_input_dim = 2
+    nn_output_dim = 2
+    epsilon = 0.01
+    reg_lamda = 0.01
+
+def generate_data():
+    np.random.seed(0)
+    X, y = datasets.make_moons(200, noise=0.20)
+    return X, y
+
+def visualize(X, y, model):
+    plot_decision_boundary(lambda x: predict(model, x), X, y)
+    plt.title("Logistic Regression")
+
+def plot_decision_boundary(pred_func, X, y):
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    h = 0.01
+    #Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    Z = pred_func(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    plt.contourf(xx, yy, Z, camp=plt.cm.Spectral)
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral)
+    plt.show()
+
+def calculate_loss(model, X, y):
+    num_examples = len(X)
+    W1, b1, W2, b2, W3, b3 = model['W1'], model['b1'], model['W2'], model['b2'], model['W3'], model['b3']
+    # Forward propagation
+    z1 = X.dot(W1) + b1
+    a1 = np.tanh(z1)
+    z2 = a1.dot(W2) + b2
+    a2 = np.tanh(z2)
+    z3 = a2.dot(W3) + b3
+    exp_scores = np.exp(z3)
+    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+    correct_logprobs = -np.log(probs[range(num_examples), y])
+    data_loss = np.sum(correct_logprobs)
+    data_loss += Config.reg_lamda / 2 * (np.sum(np.square(W1)) + np.sum(np.square(W2)) + np.sum(np.square(W3)))
+    return 1. / num_examples * data_loss
+
+def predict(model, x):
+    W1, b1, W2, b2, W3, b3 = model['W1'], model['b1'], model['W2'], model['b2'], model['W3'], model['b3']
+    z1 = x.dot(W1) + b1
+    a1 = np.tanh(z1)
+    z2 = a1.dot(W2) + b2
+    a2 = np.tanh(z2)
+    z3 = a2.dot(W3) + b3
+    exp_scores = np.exp(z3)
+    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+    return np.argmax(probs, axis=1)
+
+def build_model(X, y, nn_hdim, num_passes=20000, print_loss=False):
+    num_examples = len(X)
+    np.random.seed(0)
+    W1 = np.random.randn(Config.nn_input_dim, nn_hdim) / np.sqrt(Config.nn_input_dim)
+    b1 = np.zeros((1, nn_hdim))
+    W2 = np.random.randn(nn_hdim, nn_hdim) / np.sqrt(nn_hdim)
+    b2 = np.zeros((1, nn_hdim))
+    W3 = np.random.randn(nn_hdim, Config.nn_output_dim) / np.sqrt(nn_hdim)
+    b3 = np.zeros((1, Config.nn_output_dim))
+    model = {}
+    for i in range(0, num_passes):
+        z1 = X.dot(W1) + b1
+        a1 = np.tanh(z1)
+        z2 = a1.dot(W2) + b2
+        a2 = np.tanh(z2)
+        z3 = a2.dot(W3) + b3
+        exp_scores = np.exp(z3)
+        probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+        delta4 = probs
+        delta4[range(num_examples), y] -= 1
+        dW3 = (a2.T).dot(delta4)
+        db3 = np.sum(delta4, axis=0, keepdims=True)
+        delta3 = delta4.dot(W3.T) * (1 - np.power(a2, 2))
+        dW2 = (a1.T).dot(delta3)
+        db2 = np.sum(delta3, axis=0, keepdims=True)
+        delta2 = delta3.dot(W2.T) * (1 - np.power(a1, 2))
+        dW1 = np.dot(X.T, delta2)
+        db1 = np.sum(delta2, axis=0)
+
+        dW3 += Config.reg_lamda * dW3
+        dW2 += Config.reg_lamda * dW2
+        dW1 += Config.reg_lamda * dW1
+
+
+        #Gradient descent
+        W1 += -Config.epsilon * dW1
+        b1 += -Config.epsilon * db1
+        W2 += -Config.epsilon * dW2
+        b2 += -Config.epsilon * db2
+        W3 += -Config.epsilon * dW3
+        b3 += -Config.epsilon * db3
+
+        model = {'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2, 'W3': W3, 'b3': b3}
+
+        if print_loss and i % 1000 == 0:
+            print("Loss after iteration %i: %f" % (i, calculate_loss(model, X, y)))
+    return model
+
+def main():
+    X, y = generate_data()
+    model = build_model(X, y, 3, print_loss=True)
+    visualize(X, y, model)
+
+if __name__ == "__main__":
+    main()
+
